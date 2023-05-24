b07820223714ade4485660380cfdb5a0a14bcaf7
anto <14nicosan@gmail.com>
2023-05-24 16:04:59 +0200

    creating new_version for gnn3 not test maybe problem with device or dimension

--- Model/GNN3.py
+++ Model/GNN3.py
@@ -54,19 +54,22 @@ class ModelWithEdgeFeatures(torch.nn.Module):
             # Put a ReLU activation after each layer but not after the last one
             if message_passing_layer != self.message_passing_layers[-1]:
                 x = F.relu(x)
-            if self.use_dropout:
+            if self.use_dropout and  message_passing_layer != self.message_passing_layers[-1]:
                 x = F.dropout(x, training=self.training)
         
         return x
     
 
-class ModelWithhraph_embedding also(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels_list, edge_channels, use_dropout=True, use_batchnorm=True):
+class ModelWithgraph_embedding(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels_list, mlp_hidden_channels,edge_channels, num_classes, use_dropout=True, use_batchnorm=True, size_info = False):
         torch.manual_seed(12345)
-        super(ModelWithEdgeFeatures, self).__init__()
+        super(ModelWithgraph_embedding, self).__init__()
 
         self.use_dropout = use_dropout
         self.use_batchnorm = use_batchnorm
+        self.size_info = size_info
+        self.num_classes = num_classes
+
 
         self.message_passing_layers = torch.nn.ModuleList()
         self.batch_norm_layers = torch.nn.ModuleList()
@@ -78,6 +81,12 @@ class ModelWithhraph_embedding also(torch.nn.Module):
             if self.use_batchnorm:
                 self.batch_norm_layers.append(torch.nn.BatchNorm1d(hidden_channels))
             prev_channels = hidden_channels
+        if self.size_info:
+            self.fc1 = torch.nn.Linear(hidden_channels_list[-1] + 1, mlp_hidden_channels)
+        else:
+            self.fc1 = torch.nn.Linear(hidden_channels_list[-1], mlp_hidden_channels)
+        self.fc2 = torch.nn.Linear(mlp_hidden_channels, self.num_classes)
+        
 
 
     def forward(self, data):
@@ -87,14 +96,40 @@ class ModelWithhraph_embedding also(torch.nn.Module):
             x = message_passing_layer(x, edge_index, edge_attr)
             if self.use_batchnorm:
                 x = batch_norm_layer(x)
+
             # Put a ReLU activation after each layer but not after the last one
-            if message_passing_layer != self.message_passing_layers[-1]:
-                x = F.relu(x)
+            x = F.relu(x)
             if self.use_dropout:
                 x = F.dropout(x, training=self.training)
         
-        return x
-    
+        graph_embedd = global_add_pool(x, batch)
+        
+        if self.size_info:
+            # Concatenate size of each graph of the batch 
+            num_nodes_per_graph = torch.bincount(data.batch).view(-1, 1).float()
+            # Normalize num_node 
+            num_nodes_per_graph = num_nodes_per_graph / num_nodes_per_graph.max()
+
+            graph_embedd= torch.cat((graph_embedd, num_nodes_per_graph), dim=1)
+
+        # Two-layer MLP for classification
+
+        #maybe there is an error with the deice where it has been projected
+
+        out = torch.zeros(x.shape[0],self.num_classes)
+        for i in range(x.shape[0]):
+            node = x[i]
+            graph_idx = batch[i]
+            graph_emb = graph_embedd[graph_idx]
+            new_input = torch.cat((node, graph_emb), dim=0)
+            node_out = F.relu(self.fc1(new_input))
+            if self.use_dropout:
+                node_out = F.dropout(node_out, training=self.training)
+            node_out = self.fc2(node_out)
+            out[i,:] = node_out
+
+
+        return out 
 
 
 
