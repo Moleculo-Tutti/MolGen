7f6da66fa072349ae08352b3a4bb250a8a539b32
Benoit-Goupil <benoit.goupil@student-cs.fr>
2023-06-01 15:23:12 +0200

    final pipeline correct

--- FinalPipeline/pipeline_generation.py
+++ FinalPipeline/pipeline_generation.py
@@ -2,116 +2,57 @@ import torch
 import torch_geometric
 
 
-from utils import sample_random_subgraph_ZINC, get_model_GNN1, get_model_GNN2, get_model_GNN3, get_optimizer, one_step, create_torch_graph_from_one_atom, load_model, sample_first_atom
+from utils import GenerationModule, MolGen, tensor_to_smiles
 from tqdm import tqdm
 from rdkit import Chem
 import pandas as pd
 from path import Path
 import argparse
+import json
 
 
-def tensor_to_smiles(node_features, edge_index, edge_attr):
-    # Create an empty editable molecule
-    mol = Chem.RWMol()
-
-    # Define atom mapping
-    atom_mapping = {
-        0: 'C',
-        1: 'N',
-        2: 'O',
-        3: 'F',
-        4: 'S',
-        5: 'Cl',
-    }
-
-    # Add atoms
-    for atom_feature in node_features:
-        atom_idx = atom_feature[:6].argmax().item()
-        atom_symbol = atom_mapping.get(atom_idx)
-        atom = Chem.Atom(atom_symbol)
-        mol.AddAtom(atom)
-
-    # Define bond type mapping
-    bond_mapping = {
-        0: Chem.rdchem.BondType.AROMATIC,
-        1: Chem.rdchem.BondType.SINGLE,
-        2: Chem.rdchem.BondType.DOUBLE,
-        3: Chem.rdchem.BondType.TRIPLE,
-    }
-
-    # Add bonds
-    for start, end, bond_attr in zip(edge_index[0], edge_index[1], edge_attr):
-        bond_type_idx = bond_attr[:4].argmax().item()
-        bond_type = bond_mapping.get(bond_type_idx)
-
-        # RDKit ignores attempts to add a bond that already exists,
-        # so we need to check if the bond exists before we add it
-        if mol.GetBondBetweenAtoms(start.item(), end.item()) is None:
-            mol.AddBond(start.item(), end.item(), bond_type)
-
-    # Convert the molecule to SMILES
-    smiles = Chem.MolToSmiles(mol)
-
+def convert_to_smiles(graph):
+    smiles = []
+    for g in graph:
+        smiles.append(tensor_to_smiles(g.x, g.edge_index, g.edge_attr))
     return smiles
 
+# read the configs
+name1 = 'GNN1_charged'
+epoch1 = 0
+name2 = 'GNN2_charged'
+epoch2 = 0
+name3 = 'GNN3_charged'
+epoch3 = 0
+config1_path = Path('..') / 'Train' / 'GNN1' / 'config_GNN1.json'
+config2_path = Path('..') / 'Train' / 'GNN2' / 'config_GNN2.json'
+config3_path = Path('..') / 'Train' / 'GNN3' / 'config_GNN3.json'
 
-def full_generation(GNN1, GNN2, GNN3, zinc_df):
-    graph = create_torch_graph_from_one_atom(sample_first_atom())
-    output = torch_geometric.data.Batch.from_data_list([graph])
-    queue = [0]
-    i = 1
-    while queue and i < 100:
-        output, queue = one_step(output, queue, GNN1=GNN1, GNN2=GNN2, GNN3=GNN3, device='cuda')
-        output = torch_geometric.data.Batch.from_data_list([output])
-        i += 1  
-    return output, queue
+# Read the config as a json
 
-# Function that generate a desired number of molecules
-def generate_molecules(n_molecules, GNN1, GNN2, GNN3, zinc_df):
-    molecules = []
-    for i in tqdm(range(n_molecules)):
-        output, queue = full_generation(GNN1, GNN2, GNN3, zinc_df)
-        molecules.append(tensor_to_smiles(output.x, output.edge_index, output.edge_attr))
-    return molecules
-
+with open(config1_path, 'r') as f:
+    config1 = json.load(f)
+with open(config2_path, 'r') as f:
+    config2 = json.load(f)
+with open(config3_path, 'r') as f:
+    config3 = json.load(f)
 
 def main(args):
 
-    zinc_path = Path('rndm_zinc_drugs_clean_3.csv')
-    zinc_df = pd.read_csv(zinc_path)
+    GNN1_path = Path('.') / 'models/trained_models/checkpoint_epoch_960_GNN1_charged.pt'
+    GNN2_path = Path('.') / 'models/trained_models/checkpoint_epoch_960_GNN2_charged.pt'
+    GNN3_path = Path('.') / 'models/trained_models/checkpoint_epoch_220_GNN3_charged.pt'
 
-    GNN1_path = Path('.') / 'models/trained_models/checkpoint_epoch_247_GNN1.pt'
-    GNN2_path = Path('.') / 'models/trained_models/checkpoint_epoch_236_GNN2_test.pt'
-    GNN3_path = Path('.') / 'models/trained_models/checkpoint_epoch_110_experience_features_position.pt'
+    module = GenerationModule(config1=config1, config2=config2, config3=config3, encoding_size = 13, pathGNN1=GNN1_path, pathGNN2=GNN2_path, pathGNN3=GNN3_path)
 
-    GNN1 = get_model_GNN1(7)
-    GNN2 = get_model_GNN2(7)
-    GNN3 = get_model_GNN3(7)
-
-    optimizer_GNN1 = get_optimizer(GNN1, lr=0.0001)
-    optimizer_GNN2 = get_optimizer(GNN2, lr=0.0001)
-    optimizer_GNN3 = get_optimizer(GNN3, lr=0.0001)
-
-    GNN1, optimizer_GNN1 = load_model(GNN1_path, GNN1, optimizer_GNN1)
-    GNN2, optimizer_GNN2 = load_model(GNN2_path, GNN2, optimizer_GNN2)
-    GNN3, optimizer_GNN3 = load_model(GNN3_path, GNN3, optimizer_GNN3)
-
-    # Set all models in eval mode
-
-    GNN1.eval()
-    GNN2.eval()
-    GNN3.eval()
-
-    GNN1.to('cuda')
-    GNN2.to('cuda')
-    GNN3.to('cuda')
+    graph_batch = module.generate_mol_list(args.n_molecules)
 
 
-    molecules = generate_molecules(args.n_molecules, GNN1, GNN2, GNN3, zinc_df)
+    molecules = convert_to_smiles(graph_batch)
 
     #Save the molecule list in a csv file
     df = pd.DataFrame(molecules, columns=['SMILES'])
-    df.to_csv('generated_molecules_10000.csv', index=False)
+    df.to_csv('generated_molecules_{0}_charged.csv'.format(args.n_molecules), index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- FinalPipeline/utils.py
+++ FinalPipeline/utils.py
@@ -6,6 +6,9 @@ import torch.nn.functional as F
 import sys
 import os
 
+from rdkit import Chem
+from tqdm import tqdm
+
 cwd = os.getcwd()
 parent_dir = os.path.dirname(cwd)
 sys.path.append(parent_dir)
@@ -16,6 +19,56 @@ from Model.GNN1 import ModelWithEdgeFeatures as GNN1
 from Model.GNN2 import ModelWithEdgeFeatures as GNN2
 from Model.GNN3 import ModelWithEdgeFeatures as GNN3
 
+def tensor_to_smiles(node_features, edge_index, edge_attr):
+    # Create an empty editable molecule
+    mol = Chem.RWMol()
+
+    # Define atom mapping
+    atom_mapping = {
+        0: ('C', 0),
+        1: ('N', 0),
+        2: ('N', 1),
+        3: ('N', -1),
+        4: ('O', 0),
+        5: ('O', -1),
+        6: ('F', 0),
+        7: ('S', 0),
+        8: ('S', -1),
+        9: ('Cl', 0),
+        10: ('Br', 0),
+        11: ('I', 0)
+    }
+
+    # Add atoms
+    for atom_feature in node_features:
+        atom_idx = atom_feature[:12].argmax().item()
+        atom_symbol, charge = atom_mapping.get(atom_idx)
+        atom = Chem.Atom(atom_symbol)
+        atom.SetFormalCharge(charge)
+        mol.AddAtom(atom)
+
+    # Define bond type mapping
+    bond_mapping = {
+        0: Chem.rdchem.BondType.AROMATIC,
+        1: Chem.rdchem.BondType.SINGLE,
+        2: Chem.rdchem.BondType.DOUBLE,
+        3: Chem.rdchem.BondType.TRIPLE,
+    }
+
+    # Add bonds
+    for start, end, bond_attr in zip(edge_index[0], edge_index[1], edge_attr):
+        bond_type_idx = bond_attr[:4].argmax().item()
+        bond_type = bond_mapping.get(bond_type_idx)
+
+        # RDKit ignores attempts to add a bond that already exists,
+        # so we need to check if the bond exists before we add it
+        if mol.GetBondBetweenAtoms(start.item(), end.item()) is None:
+            mol.AddBond(start.item(), end.item(), bond_type)
+
+    # Convert the molecule to SMILES
+    smiles = Chem.MolToSmiles(mol)
+
+    return smiles
 
 def sample_random_subgraph_ZINC(pd_dataframe, start_size):
     indice = random.choice(pd_dataframe.index)
@@ -26,20 +79,22 @@ def sample_random_subgraph_ZINC(pd_dataframe, start_size):
 
     return subgraph_data, terminal_node_info, id_map
 
-def sample_first_atom():
-    prob_dict = {'6': 0.7385023585929047, 
-                 '8': 0.1000143018658728, 
-                 '7': 0.12239949901813525, 
-                 '9': 0.013786373862576426, 
-                 '16': 0.017856330814654413,
-                 '17': 0.007441135845856433}
+def sample_first_atom(encoding = 'reduced'):
+    if encoding == 'reduced' or encoding == 'charged':
+        prob_dict = {'60': 0.7385023585929047, 
+                    '80': 0.1000143018658728, 
+                    '70': 0.12239949901813525, 
+                    '90': 0.013786373862576426, 
+                    '160': 0.017856330814654413,
+                    '170': 0.007441135845856433}
+
     
     return random.choices(list(prob_dict.keys()), weights=list(prob_dict.values()))[0]
 
-def create_torch_graph_from_one_atom(atom):
+def create_torch_graph_from_one_atom(atom, encoding_option='reduced'):
     num_atom = int(atom)
 
-    atom_attribute = node_encoder(num_atom, encoding_option='reduced')
+    atom_attribute = node_encoder(num_atom, encoding_option=encoding_option)
     # Create graph
     graph = torch_geometric.data.Data(x=atom_attribute.view(1, -1), edge_index=torch.empty((2, 0), dtype=torch.long), edge_attr=torch.empty((0, 4)))
 
@@ -54,9 +109,8 @@ def load_model(checkpoint_path, model, optimizer):
     return model, optimizer
 
 def get_model_GNN1(config, encoding_size):
-    if config['feature_position']:
-        encoding_size += 1
-    return GNN1(in_channels=encoding_size,
+
+    return GNN1(in_channels=encoding_size + int(config['feature_position']),
                 hidden_channels_list=config["GCN_size"],
                 mlp_hidden_channels=config['mlp_hidden'],
                 edge_channels=4, 
@@ -65,9 +119,8 @@ def get_model_GNN1(config, encoding_size):
                 size_info=config['use_size'])
 
 def get_model_GNN2(config, encoding_size):
-    if config['feature_position']:
-        encoding_size += 1
-    return GNN2(in_channels=encoding_size, 
+
+    return GNN2(in_channels=encoding_size + int(config['feature_position']), 
                 hidden_channels_list=config["GCN_size"],
                 mlp_hidden_channels=config['mlp_hidden'],
                 edge_channels=4, 
@@ -75,9 +128,8 @@ def get_model_GNN2(config, encoding_size):
                 use_dropout=config['use_dropout'])
 
 def get_model_GNN3(config, encoding_size):
-    if config['feature_position']:
-        encoding_size += 1
-    return GNN3(in_channels=encoding_size, 
+
+    return GNN3(in_channels=encoding_size + int(config['feature_position']), 
                 hidden_channels_list=config["GCN_size"],
                 edge_channels=4, 
                 use_dropout=config['use_dropout'])
@@ -112,17 +164,15 @@ def add_edge_or_node_to_graph(graph, initial_node, edge_attr, other_node=None, n
     # Create new edge
     new_edge = torch.tensor([[initial_node.item(), other_node.item()], 
                              [other_node.item(), initial_node.item()]], dtype=torch.long)
-
     # Add new edge and its attribute to graph
     graph.edge_index = torch.cat([graph.edge_index, new_edge], dim=1)
     graph.edge_attr = torch.cat([graph.edge_attr, new_edge_attr], dim=0)
 
+    graph.batch = torch.zeros(graph.x.size(0), dtype=torch.long)
+
     return graph
 
 def select_node(tensor):
-    
-    # Vérifier que le tenseur est de dimension 2 et que la deuxième dimension est de taille 4
-    assert len(tensor.shape) == 2 and tensor.shape[1] == 5, "Le tenseur doit être de dimension 2 et la deuxième dimension doit être de taille 4"
 
     # Somme sur les 3 premières dimensions de chaque vecteur
     sum_on_first_three_dims = tensor[:, :3].sum(dim=1)
@@ -205,7 +255,7 @@ def one_step(input_graph, queue : list, GNN1, GNN2, GNN3, device):
         #sample edge
         predicted_edge = torch.multinomial(selected_edge_distribution, 1).item()
         
-        if predicted_edge == 4:
+        if predicted_edge == 3:
             #Stop
             return new_graph, queue
         
@@ -215,5 +265,151 @@ def one_step(input_graph, queue : list, GNN1, GNN2, GNN3, device):
 
         output_graph = add_edge_or_node_to_graph(new_graph, graph1.x.size(0), encoded_predicted_edge, other_node=current_node + max_index+1)
         return output_graph, queue
+
+class MolGen():
+    def __init__(self, GNN1, GNN2, GNN3, encoding_size, feature_position, device, save_intermidiate_states = False):
+        mol_graph = create_torch_graph_from_one_atom(sample_first_atom(), encoding_option='charged')
+        self.mol_graph = torch_geometric.data.Batch.from_data_list([mol_graph])
+        self.queue = [0]
+        self.GNN1 = GNN1
+        self.GNN2 = GNN2
+        self.GNN3 = GNN3
+        self.device = device
+        self.feature_position = feature_position
+        self.encoding_size = encoding_size
+
+    def one_step(self):
+        with torch.no_grad():
+
+            current_node = self.queue[0]
+            graph1 = self.mol_graph.clone()
+
+            # add a one to the current node in the last column
+            graph1.x[current_node, -1] = 1
+
+            if self.feature_position:
+                #add feature position
+                graph1.x = torch.cat([graph1.x, torch.zeros(graph1.x.size(0), 1)], dim=1)
+                graph1.x[0:current_node, -1] = 1
+            prediction = self.GNN1(graph1.to(self.device))
+            # Sample next node from prediction
+            predicted_node = torch.multinomial(F.softmax(prediction, dim=1), 1).item()
+            if predicted_node == self.encoding_size - 1:
+                #Stop 
+                self.queue.pop(0)
+                return
+                
+            # Encode next node
+            encoded_predicted_node = torch.zeros(prediction.size(), dtype=torch.float)
+            encoded_predicted_node[0, predicted_node] = 1
+
+        
+            self.queue.append(graph1.x.size(0)) # indexing starts from 0
+
+            #GNN2 
+
+            graph2 = self.mol_graph.clone()
+
+            # add a one to the current node in the last column
+            graph2.x[current_node, -1] = 1
+
+            if self.feature_position:
+                #add feature position
+                graph2.x = torch.cat([graph2.x, torch.zeros(graph2.x.size(0), 1)], dim=1)
+                graph2.x[0:current_node, -1] = 1
+
+            #add zeros to the neighbor
+            encoded_predicted_node = torch.cat([encoded_predicted_node, torch.zeros(1, 1)], dim=1)
+            graph2.neighbor = encoded_predicted_node
+
+            prediction2 = self.GNN2(graph2.to(self.device))
+
+            predicted_edge = torch.multinomial(F.softmax(prediction2, dim=1), 1).item()
+            encoded_predicted_edge = torch.zeros(prediction2.size(), dtype=torch.float)
+            encoded_predicted_edge[0, predicted_edge] = 1
+
+            #GNN3
+
+            # Add the node and the edge to the graph
+            new_graph = add_edge_or_node_to_graph(self.mol_graph.clone(), current_node, encoded_predicted_edge, new_node_attr = encoded_predicted_node[:, :-1])
+            graph3 = new_graph.clone()
+
+            # Add a one of the last node that are going to possibly bond to another node
+            graph3.x[-1, -1] = 1
+
+            if self.feature_position:
+                #Add a new column indicating the nodes that have been finalized
+                graph3.x = torch.cat([graph3.x, torch.zeros(graph3.x.size(0), 1)], dim=1)
+                graph3.x[0:current_node, -1] = 1
+
+            mask = torch.cat((torch.zeros(current_node + 1), torch.ones(len(graph3.x) - current_node - 1)), dim=0).bool()
+            mask[-1] = False
+            graph3.mask = mask
+            prediction3 = self.GNN3(graph3.to(self.device))
+            softmax_prediction3 = F.softmax(prediction3, dim=1)[graph3.mask]
+            if softmax_prediction3.size(0) == 0:
+                #Stop
+                self.mol_graph = new_graph
+                return
+
+            selected_edge_distribution, max_index = select_node(softmax_prediction3)
+
+             #sample edge
+            predicted_edge = torch.multinomial(selected_edge_distribution, 1).item()
+
+            if predicted_edge == 3:
+                #Stop
+                self.mol_graph = new_graph
+                return
+
+            encoded_predicted_edge = torch.zeros(prediction2.size(), dtype=torch.float)
+            encoded_predicted_edge[0, predicted_edge] = 1
+
+
+            output_graph = add_edge_or_node_to_graph(new_graph, graph1.x.size(0), encoded_predicted_edge, other_node=current_node + max_index+1)
+
+            self.mol_graph = output_graph
+
+    def full_generation(self):
+        while len(self.queue) > 0:
+            self.one_step()
+
+
+
+
+class GenerationModule():
+    def __init__(self, config1, config2, config3, encoding_size, pathGNN1, pathGNN2, pathGNN3):
+        self.config1 = config1
+        self.config2 = config2
+        self.config3 = config3
+        self.encoding_size = encoding_size
+        self.feature_position = config1["feature_position"]
+        self.GNN1 = get_model_GNN1(config1, encoding_size)
+        self.GNN2 = get_model_GNN2(config2, encoding_size)
+        self.GNN3 = get_model_GNN3(config3, encoding_size)
+
+        self.optimizer_GNN1 = torch.optim.Adam(self.GNN1.parameters(), lr=config1["lr"])
+        self.optimizer_GNN2 = torch.optim.Adam(self.GNN2.parameters(), lr=config2["lr"])
+        self.optimizer_GNN3 = torch.optim.Adam(self.GNN3.parameters(), lr=config3["lr"])
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.GNN1_model, self.optimizer_GNN1 = load_model(pathGNN1, self.GNN1, self.optimizer_GNN1)
+        self.GNN2_model, self.optimizer_GNN2 = load_model(pathGNN2, self.GNN2, self.optimizer_GNN2)
+        self.GNN3_model, self.optimizer_GNN3 = load_model(pathGNN3, self.GNN3, self.optimizer_GNN3)
+
+        self.GNN1_model.to(self.device)
+        self.GNN2_model.to(self.device)
+        self.GNN3_model.to(self.device)
+
+        self.GNN1_model.eval()
+        self.GNN2_model.eval()
+        self.GNN3_model.eval()
     
+    def generate_mol_list(self, n_mol):
+        mol_list = []
+        for i in tqdm(range(n_mol)):
+            mol = MolGen(self.GNN1_model, self.GNN2_model, self.GNN3_model, self.encoding_size, self.feature_position, self.device)
+            mol.full_generation()
+            mol_list.append(mol.mol_graph)
+        return mol_list
 
